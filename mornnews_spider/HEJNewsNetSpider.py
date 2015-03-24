c9ca9c6cc8a5bcc665efc6856934afef3fb7c0c1
zhang.jj <wuse_zjj@163.com>
2015-03-24 17:44:47 +0800

    sourceeditor

--- mornnews_spider/HEJNewsNetSpider.py
+++ mornnews_spider/HEJNewsNetSpider.py
@@ -15,20 +15,9 @@ def crawMorningDailyNews(link):
     for  context in listContext:
          title = context.find_element_by_class_name('title').text
          linkUrl = context.find_element_by_class_name('title').get_attribute('href')
+         imageUrl = context.find_element_by_tag_name('img').get_attribute('data-original').split('!')[0]
          pubDate = time.strftime("%Y-%m-%d %X",time.localtime())
-         browsors = webdriver.PhantomJS()
-         browsors.get(linkUrl)
-         try:
-             mainContext = browsors.find_element_by_class_name('article-content')
-         except NoSuchElementException,e:
-            continue
-         imageUrl =None
-         try:
-            imageUrl = mainContext.find_element_by_class_name('article-content').find_element_by_tag_name('img')
-         except NoSuchElementException,e:
-            continue
-         imageUrl =imageUrl.get_attribute('src')
-         descriptContext = mainContext.find_element_by_tag_name('p').text
+         descriptContext = ''
          currentArray.append([str(uuid.uuid1()),linkUrl,imageUrl,title,pubDate,descriptContext,'STOCK','HEJNET'])
 
     return currentArray
