8870b1f7bf034d1bae093be3f35b6b824508a3d6
zhang.jj <wuse_zjj@163.com>
2015-03-24 16:17:41 +0800

    sourceeditor

--- mornnews_spider/HEJNewsNetSpider.py
+++ mornnews_spider/HEJNewsNetSpider.py
@@ -1,61 +1,38 @@
-import HEJNewsNetSpiderUtils
+from  selenium import webdriver
+import sys
+sys.path.append("../commonutils_spider/")
+import CommonsMysqlUtils
 import time
 import uuid
 
+def crawMorningDailyNews(link):
+    currentArray = []
+    browsor = webdriver.PhantomJS()
+    browsor.get(link)
+    mainContext = browsor.find_element_by_id('search-result')
+    listContext = mainContext.find_elements_by_class_name('news')
+    for  context in listContext:
+         title = context.find_element_by_class_name('title').text
+         linkUrl = context.find_element_by_class_name('title').get_attribute('href')
+         imageUrl = context.find_element_by_tag_name('img').get_attribute('src')
+         pubDate = time.strftime("%Y-%m-%d %X",time.localtime())
+         descriptContext = ''
+         currentArray.append([str(uuid.uuid1()),linkUrl,imageUrl,title,pubDate,descriptContext,'STOCK','HEJNET'])
 
-def crawMorningDailyNews(linkUrl,WebNet):
-    currentList = []
-    startContext = HEJNewsNetSpiderUtils.returnStartContext(linkUrl,'<div class="view-content">')
-    i = 0
-    while  i < 20 :
-        targetContext = HEJNewsNetSpiderUtils.divisionTarget(startContext,'<div class="views-row','</div><!-- media body end-->')
-        startContext = targetContext['nextContext']
-        currentContext =  targetContext['targetContext']
-        currentContext = HEJNewsNetSpiderUtils.removeSpecialCharacter(currentContext)
-        imageUrl = HEJNewsNetSpiderUtils.filterContextByTarget(currentContext,'data-original="','"alt')
-        linkUrl = WebNet + HEJNewsNetSpiderUtils.filterContextByTarget(currentContext,'<aclass="pull-leftnews-img"href="','"target="_blank">')
-        title = HEJNewsNetSpiderUtils.filterAfterContext(currentContext, 'alt')
-        pubDate = HEJNewsNetSpiderUtils.filterAfterContext(title, '</a></h2></header>')
-        pubDate = HEJNewsNetSpiderUtils.filterAfterContext(pubDate,'</a></span>')
-        title = HEJNewsNetSpiderUtils.filterContextByTarget(title,'target="_blank">','</a></h2></header>')
-        flag = False
-        if 'china' in pubDate:
-            flag = True    
-        pubDate = HEJNewsNetSpiderUtils.filterContextByTarget(pubDate,'<spanclass="meta-item">','</span>')
-        pubDate = pubDate[:4]+"-"+pubDate[7:9]+'-'+pubDate[12:14]+' '+pubDate[17:]
-        currentTime = time.strftime("%Y-%m-%d",time.localtime())
-        descriptContext = HEJNewsNetSpiderUtils.filterContextByTarget(currentContext,'<divclass="media-content">','</div></div><!--mediabodyend-->')
-        if flag:
-            if pubDate[:10] == currentTime:
-                currentList.append([str(uuid.uuid1()),linkUrl,imageUrl,title,pubDate,descriptContext,'CHINA','HEJNET'])
-        else:
-            if pubDate[:10] == currentTime:
-                currentList.append([str(uuid.uuid1()),linkUrl,imageUrl,title,pubDate,descriptContext,'EUROPE','HEJNET'])
-        i +=1
-    return currentList
+    return currentArray
 
 
 def writeMorningDailyNews():
-    link = 'http://wallstreetcn.com/topnews'
-    webNet = 'http://wallstreetcn.com'
-    currentList =  crawMorningDailyNews(link,webNet)
-    conn = HEJNewsNetSpiderUtils.getMySQLConn()
-    cursor = conn.cursor()
-    try:
-        cursor.execute("DELETE  FROM  MORNING_FINANCENEWS_RESOURCE_TABLE  WHERE  SOURCEFLAG = 'HEJNET'")
-        conn.commit()
-    except conn.Error,e:
-        print "Mysql Error %d: %s" % (e.args[0], e.args[1])
-        conn.rollback()
-    formatSQL = 'INSERT MORNING_FINANCENEWS_RESOURCE_TABLE (KEYID,LINKURL,IMAGEURL,TITLE,PUBDATE,DESCRIPTCONTEXT,NEWSFLAG,SOURCEFLAG) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)'
-    try:
-        cursor.executemany(formatSQL,currentList)
-        conn.commit()
-    except conn.Error,e:
-        print "Mysql Error %d: %s" % (e.args[0], e.args[1])
-        conn.rollback()
-    cursor.close()
-    conn.close()
+    link = 'http://wallstreetcn.com/news?cid=17'
+    currentArray =  crawMorningDailyNews(link)
+    dbManager = CommonsMysqlUtils._dbManager
+    SQL = "DELETE  FROM  MORNING_FINANCENEWS_RESOURCE_TABLE  WHERE  SOURCEFLAG = 'HEJNET'  AND  NEWSFLAG='STOCK' "
+    dbManager.executeUpdateOrDelete(SQL)
+
+    formatSQL = ' INSERT MORNING_FINANCENEWS_RESOURCE_TABLE' \
+                ' (KEYID,LINKURL,IMAGEURL,TITLE,PUBDATE,DESCRIPTCONTEXT,NEWSFLAG,SOURCEFLAG)' \
+                ' VALUES (%s,%s,%s,%s,%s,%s,%s,%s)'
+    dbManager.executeManyInsert(formatSQL,currentArray)
     
 if __name__=='__main__':
     writeMorningDailyNews()
--- mornnews_spider/XHNewsNetSpider.py
+++ mornnews_spider/XHNewsNetSpider.py
@@ -9,29 +9,25 @@ def  crawXHNewsNetDataSource(link):
      currentArray = []
      browsor = webdriver.PhantomJS()
      browsor.get(link)
-     mainDiv = browsor.find_element_by_id('showData2')
-     print  mainDiv
+     mainContext = browsor.find_element_by_id('tabCon')
+     mainDiv = mainContext.find_element_by_id('showData0')
      listDiv = mainDiv.find_elements_by_class_name('clearfix')
      print listDiv
      for div in listDiv:
-         title = div.find_element_by_tag_name('h3').text
-         print title
-         linkUrl = div.find_element_by_class_name('imgs')\
-                .find_element_by_tag_name('a').get_attribute('href')
-         imageUrl = div.find_element_by_class_name('imgs')\
-                .find_element_by_tag_name('img').get_attribute('src')
-         descriptContext = div.find_element_by_class_name('imgs')\
-                .find_element_by_class_name('summary').text
+         #title = div.find_element_by_tag_name('h3').text
+         descriptContext = div.find_element_by_class_name('summary').text
+         print  descriptContext
          createDate = div.find_element_by_class_name('time').text
          pubDate = time.strftime("%Y-%m-%d",time.localtime())
          print createDate
          if createDate == pubDate:
-           currentArray.append([str(uuid.uuid1()),linkUrl,imageUrl,title,pubDate,descriptContext,'CHINA','XHNET'])
+            print '---'
+            #currentArray.append([str(uuid.uuid1()),linkUrl,imageUrl,title,pubDate,descriptContext,'CHINA','XHNET'])
      return currentArray
 
 
 def writeXHNewsNetDataSource():
-    link = 'http://www.xinhuanet.com/fortune/index.htm'
+    link = 'http://www.news.cn/finance/'
 
     #dbManager = CommonsMysqlUtils._dbManager
     SQL = " DELETE  FROM  MORNING_FINANCENEWS_RESOURCE_TABLE  WHERE  SOURCEFLAG = 'XHNET' " \
